68b14610c768b2efd71476a4cde1beec852dcdc5
emersonbc98 <56715200+emersonbc98@users.noreply.github.com>
2020-04-18 15:18:04 +0800

    Update PlayerMovement.cs
    Completed jetpack and fixed up other code

--- Assets/Scripts/PlayerController/PlayerMovement.cs
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
     private float sprint = 1f;
     public float maxFuel = 100f;
     public float jetpackBoost = 30f;
-    public float noJetpackBoost = 1f;
+    //public float noJetpackBoost = 1f; <<< We do not need this
     public float jumpSpeed = 10f;
     public float fuel = 10f;
 
@@ -64,26 +64,64 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && fuel > 0)
+        //GetKeyDown only triggers once, meaning the fuel will only drop once and nothing else will happen.
+        //GetKey will check every single frame if the key is being held, so this works better
+        if (Input.GetKey(KeyCode.E) && fuel > 0)
+        {
+            //Check if we've not set flying to True while the key is held and we have the fuel
+            //This will only trigger when we start holding E.
+            if (!flying)
+            {
+                flying = true;
+            }
+        }
+        //The Else statement here triggers when we've stopped holding E OR fuel has dropped below 0.
+        else
+        {
+            //We don't need a separate velocity mod here as the gravity code below will drag us back to the ground
+            flying = false;
+            Debug.Log("Dropping");
+
+            //Fuel got added every frame, which meant we could fly infinitely as the jetpack would just turn on and off. So I changed the value here.
+            if (fuel < 100)
+            {
+                //I added a check here to see if we are grounded, so fuel charges faster when we're on the ground
+                if (isGrounded)
+                {
+                    //Multiplying this ammount by time.deltaTime means it's fairly accurate
+                    fuel += 30f * Time.deltaTime;
+                }
+                else
+                {
+                    //We're in the air, so charge fuel slower
+                    //Multiplying this ammount by time.deltaTime means it's fairly accurate
+                    fuel += 0.01f * Time.deltaTime;
+                }
+
+                //Before we print the fuel, let's make sure it's not over filled and cap it.
+                if (fuel > 100)
+                {
+                    fuel = 100;
+                }
+
+                print(fuel);
+            }
+        }
+
+        //This is where we check if we're flying and apply our velocity as desired
+        if (flying)
         {
             velocity.y = jetpackBoost;
-            fuel -= 10;
-            print(fuel);
-            flying = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.E) || fuel < maxFuel && isGrounded == true)
-        {
-            fuel += 10;
-            print(fuel);
-
-            if (flying)
+            if (fuel > 0)
             {
-                velocity.y = noJetpackBoost;
-                flying = false;
+                //Removing 10 fuel a frame typically means losing all your fuel instantly
+                //Multiplying this ammount by time.deltaTime means it's fairly accurate
+                fuel -= 20f * Time.deltaTime;
+                print(fuel);
             }
-
         }
+
+        Debug.Log("Flying = " + flying);
 
         velocity.y += gravity * Time.deltaTime;
 
