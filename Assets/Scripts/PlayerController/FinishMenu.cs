51a270809aa5852faa7a7dc9f7a2073cf0516a69
emersonbc98 <56715200+emersonbc98@users.noreply.github.com>
2020-05-02 13:00:07 +0800

    Finish and Instruction Menu

--- Assets/Scripts/PlayerController/FinishMenu.cs
+++ Assets/Scripts/PlayerController/FinishMenu.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Instructions : MonoBehaviour
+public class FinishMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,9 @@ public class Instructions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.activeInHierarchy)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
